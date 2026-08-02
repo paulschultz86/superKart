60a3900ac25074522fd51e78435ec3cfc87d2591
Paul Schultz <paulschultz86@gmail.com>
2026-08-02 16:05:45 -0600

    Update app.py

--- frontend/app.py
+++ frontend/app.py
@@ -1,4 +1,3 @@
-
 import streamlit as st
 import pandas as pd
 import requests
@@ -14,14 +13,21 @@ st.write(
 
 # Input fields for product and store data
 Product_Weight = st.number_input("Product Weight", min_value=0.0, value=12.66)
+
 Product_Sugar_Content = st.selectbox("Product Sugar Content", ["Low Sugar", "Regular", "No Sugar"])
+
 Product_Allocated_Area = st.number_input("Product Allocated Area", min_value=0.0, value=0.027)
+
 Product_MRP = st.number_input("Product MRP", min_value=0.0, value=117.08)
+
 Store_Size = st.selectbox("Store Size", ["Small", "Medium", "High"])
+
 Store_Location_City_Type = st.selectbox("Store Location City Type", ["Tier 1", "Tier 2", "Tier 3"])
+
 Store_Type = st.selectbox("Store Type", ["Supermarket Type1", "Supermarket Type2", "Supermarket Type3", "Departmental Store", "Food Mart"])
-Product_Id_char = st.selectbox("Product ID Character", ["FD", "DR", "NC"])
+
 Store_Age_Years = st.number_input("Store Age (Years)", min_value=0, value=16)
+
 Product_Type_Category = st.selectbox("Product Type Category", ["Perishables", "Non Perishables"])
 
 # Create JSON payload
@@ -33,14 +39,12 @@ product_data = {
     "Store_Size": Store_Size,
     "Store_Location_City_Type": Store_Location_City_Type,
     "Store_Type": Store_Type,
-    "Product_Id_char": Product_Id_char,
     "Store_Age_Years": Store_Age_Years,
     "Product_Type_Category": Product_Type_Category
 }
 
 # Single Prediction
 if st.button("Predict", type='primary'):
-
     response = requests.post(
         f"{BACKEND_URL}/v1/predict",
         json=product_data
@@ -64,7 +68,6 @@ uploaded_file = st.file_uploader(
 if uploaded_file is not None:
 
     if st.button("Predict for Batch", type='primary'):
-
         response = requests.post(
             f"{BACKEND_URL}/v1/predictbatch",
             files={"file": uploaded_file}
